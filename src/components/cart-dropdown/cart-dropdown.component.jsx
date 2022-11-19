fc5872e491f7af4f0170fca164c19ac05594a5ee
Ellianel <michal93nieklan@gmail.com>
2022-11-19 18:31:04 +0100

    applied styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,33 +1,37 @@
-import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { CartContext } from '../../contexts/cart.context'
+import { CartContext } from '../../contexts/cart.context';
 
-import Button from '../button/button.component'
-import CartItem from '../cart-item/cart-item.component'
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-import { CartDropdownContainer, EmptyMsg, CartItems } from './cart-dropdown.styles'
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
-	const { cartItems } = useContext(CartContext)
-	const navigate = useNavigate()
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-	const goToCheckoutHandler = () => {
-		navigate('/checkout')
-	}
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
-	return (
-		<CartDropdownContainer>
-			<CartItems>
-				{cartItems.length ? (
-					cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
-				) : (
-					<EmptyMsg>Your cart is empty</EmptyMsg>
-				)}
-			</CartItems>
-			<Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-		</CartDropdownContainer>
-	)
-}
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,42 +1,42 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 
-import Directory from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component'
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
+	const categories = [
+		{
+			id: 1,
+			title: 'boots',
+			imageUrl: 'https://i.ibb.co/fQ9NFH9/Nike-boots-2.jpg',
+		},
+		{
+			id: 2,
+			title: 'football balls',
+			imageUrl: 'https://i.ibb.co/Y7YhCZt/2022-world-cup-adidas-al-rihla-official-match-ball.png',
+		},
+		{
+			id: 3,
+			title: 'jerseys',
+			imageUrl: 'https://i.ibb.co/nP576ZG/Lewy.webp',
+		},
+		{
+			id: 4,
+			title: 'kits',
+			imageUrl: 'https://i.ibb.co/bLQG6cQ/RealM.jpg',
+		},
+		{
+			id: 5,
+			title: 'accesories',
+			imageUrl: 'https://i.ytimg.com/vi/wXwfIjrjHpo/hqdefault.jpg',
+		},
+	]
 
-  return (
-    <div>
-      <Directory categories={categories} />
-      <Outlet />
-    </div>
-  );
-};
+	return (
+		<div>
+			<Directory categories={categories} />
+			<Outlet />
+		</div>
+	)
+}
 
-export default Home;
+export default Home
