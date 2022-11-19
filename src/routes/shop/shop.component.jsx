8c8742c71a302cd7f8d21d8afc52d93a25412f72
Ellianel <michal93nieklan@gmail.com>
2022-11-19 16:53:24 +0100

    applied styled components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import CategoriesPreview from '../categories-preview/categories-preview.component'
+import { Routes, Route } from 'react-router-dom';
 
-import './shop.styles.scss'
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+
+import './shop.styles.scss';
 
 const Shop = () => {
-	return (
-		<Routes>
-			<Route index element={<CategoriesPreview />} />
-		</Routes>
-	)
-}
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
 
-export default Shop
+export default Shop;
